ff112a292da41a2ed55e7018f19a7c0df17a4655
Steven Leishman <18265sl@hvhs.school.nz>
2021-02-28 08:34:20 +0000

    timer that counts to 30 using setInterval

--- t11_timer.js
+++ t11_timer.js
@@ -1,8 +1,23 @@
 /********************************************************/
 //
 /********************************************************/
+var timer;
+var counter = 0;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400,400)
+  timer = createP("timer");
+  setInterval(playTimer,500)
+}
+
+function playTimer (){
+  timer.html(counter);
+  counter++;
+  if(counter > 30){
+    counter--;
+  } else if(counter < 0){
+    counter++;
+  }
 }
 
 function draw() {
