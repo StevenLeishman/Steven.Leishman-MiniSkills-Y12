489b7d0327b4a36a8e435e672f072613dcb68598
Steven Leishman <18265sl@hvhs.school.nz>
2021-03-01 00:10:33 +0000

    Added timer to task 11

--- t11_timer.js
+++ t11_timer.js
@@ -1,25 +1,37 @@
 /********************************************************/
 //
 /********************************************************/
-var timer;
+var timeUp;
+var timeDown;
 var counter = 0;
+var counter2 = 30;
 
 function setup() {
-  createCanvas(400,400)
-  timer = createP("timer");
-  setInterval(playTimer,500)
+  createCanvas(400, 400)
+  background(255,255,255)
+  timeUp = createP("timeUp");
+  timeUp.style("font-size", "50px")
+  timeDown = createP("timeDown");
+  timeDown.style("font-size", "50px")
+  setInterval(playTimer, 250)
 }
 
-function playTimer (){
-  timer.html(counter);
+function playTimer() {
+  timeUp.html("timeUp = " + counter);
   counter++;
-  if(counter > 30){
-    counter--;
-  } else if(counter < 0){
-    counter++;
+  if (counter > 30) {
+    background(200, 30, 200)
+    counter = 30;
+    countDown();
   }
 }
 
-function draw() {
-  background(220);
+function countDown() {
+  timeUp.html("timeUp");
+  timeDown.html("timeDown = " + counter2);
+  counter2--;
+  if (counter2 < 0) {
+    background(0, 0, 0)
+    counter2 = 0;
+  }
 }
