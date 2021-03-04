39b247be4e1ab64928e237b61d5f8c81b7ad1df9
Steven Leishman <18265sl@hvhs.school.nz>
2021-03-04 01:03:50 +0000

    tidied up code and added clicking

--- t13_click_ball.js
+++ t13_click_ball.js
@@ -1,17 +1,75 @@
 /********************************************************/
 //
 /********************************************************/
-const NUMBALLS = 4;
+const NUMBALLS = 6;
 var ballsArray = [];
-var velArray = [10, 9, 8, 7, 6, -6, -7, -8, -9, -10];
-var score
+var velArray = [3, 4, 5, 6, -3, -4, -5, -6];
+var levelArray = [1, 1.5, 2.5, 4]
+var score = 0;
+var scoreText;
 
-
-//SETUP
+/*******************************************************/
+//function setup()
+//Called by p5.js once when the program runs
+/*******************************************************/
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  scoreText = createP("Score : ");
+  createBall(NUMBALLS);
+}
 
-  for (var i = 0; i < NUMBALLS; i++) {
+/*******************************************************/
+//function draw()
+//Called by p5.js 60 times per second
+/*******************************************************/
+function draw() {
+  background(200, 200, 200, 100);
+  scoreText.html("Score : " + score)
+  for (var i = 0; i < ballsArray.length; i++) {
+    ballsArray[i].movement()
+    ballsArray[i].bounce()
+    ballsArray[i].display()
+  }
+}
+
+/*******************************************************/
+//function mousePressed()
+//Called whenever the mouse is clicked
+/*******************************************************/
+function mousePressed() {
+  checkBallHit()
+}
+
+/*******************************************************/
+//function checkBallHit()
+//Checks if the ball is hit then adds to score and deletes
+//Called by: mousePressed()
+//Input: N/A
+//Returns: N/A
+/*******************************************************/
+function checkBallHit() {
+  var ballHit = false;
+  for (var i = 0; i < ballsArray.length; i++) {
+    var px2Ball = dist(ballsArray[i].posX, ballsArray[i].posY, mouseX, mouseY);
+    if (px2Ball <= ballsArray[i].diameter / 2) {
+      ballHit = true;
+      ballsArray.splice(i, 1);
+    }
+  }
+  if (ballHit == true) {
+    score++;
+  }
+}
+/*******************************************************/
+//function createBall
+//Creates the balls specified when called
+//Called by: setup()
+//Input: _ballAmount
+//Returns: N/A
+/*******************************************************/
+function createBall(_ballAmount) {
+
+  for (var i = 0; i < _ballAmount; i++) {
     ballsArray[i] = {
       posX: width / 2,
       posY: height / 2,
@@ -27,73 +85,40 @@ function setup() {
         ellipse(this.posX, this.posY, this.diameter);
       },
       movement: function () {
-        //Adjusts position of ball 1
-        this.posY = this.posY + this.speedY;
-        this.posX = this.posX + this.speedX;
+        //Adjusts position of the ball by the speed
+        this.posY = this.posY + this.speedY * levelArray[0];
+        this.posX = this.posX + this.speedX * levelArray[0];
       },
       bounce: function () {
         let rad = this.diameter / 2;
-        //checks if ball1 is off screen on X axis
+        //checks if the ball is off screen on X axis
         if (this.posX >= (width - (rad))) {
-          //reverse the speed of the ball1 and adjusts the position
+          //reverses the speeds of the balls and adjusts the position when hits the edge
           this.speedX = this.speedX * -1;
           this.posX = (width - (this.diameter / 2))
 
-          //check if it is off the top of the screen  
+          //check if balls it is off the top of the screen  
         } else if (this.posX <= (this.diameter / 2)) {
           //reverses the speed and adjusts the position
           this.speedX = this.speedX * -1;
           this.posX = this.diameter / 2;
         }
 
-        //checks if ball1 is off screen on Y axis
+        //checks if ball is off screen on Y axis
         if (this.posY >= (height - (this.diameter / 2))) {
-          //reverse the speed of the ball1 and adjusts the position
+          //reverse the speed of the ball and adjusts the position
           this.speedY = this.speedY * -1;
           this.posY = height - this.diameter / 2;
 
         } else if (this.posY <= this.diameter / 2) {
-          //reverse the speed of the ball1 and adjusts the position
+          //reverse the speed of the ball and adjusts the position
           this.speedY = this.speedY * -1;
           this.posY = this.diameter / 2;
-
         }
-
       }
     }
   }
 }
-
-//
-function draw() {
-  background(200, 200, 200, 100);
-  checkBallHit()
-  for (var i = 0; i < ballsArray.length; i++) {
-    ballsArray[i].movement()
-    ballsArray[i].bounce()
-    ballsArray[i].display()
-  }
-}
-
-
-
-
 /*******************************************************/
-//function checkBallHit()
-//Checks if the ball is hit then adds to score and deletes
-//Input: N/A
-//Returns: N/A
+// END OF APP
 /*******************************************************/
-function checkBallHit() {
-  var ballHit = false;
-  for (var i = 0; i < ballsArray.length; i++) {
-    var px2Ball = dist(ballsArray[i].posX, ballsArray[i].posY, mouseX, mouseY);
-    if (px2ball <= ballsArray[i].diameter / 2) {
-      ballHit = true;
-      ballsArray.splice(i, 1);
-    }
-  }
-  if (ballHit == true) {
-    score++;
-  }
-}
